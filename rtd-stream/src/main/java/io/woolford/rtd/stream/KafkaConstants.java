9ec3579244fc42bdecef160204183b139e327b02
alexwoolford <alex@woolford.io>
2020-03-09 00:34:51 -0600

    added second schema registry URL

--- rtd-stream/src/main/java/io/woolford/rtd/stream/KafkaConstants.java
+++ rtd-stream/src/main/java/io/woolford/rtd/stream/KafkaConstants.java
@@ -4,6 +4,6 @@ public interface KafkaConstants {
 
     public static String KAFKA_BROKERS = "cp01.woolford.io:9092,cp02.woolford.io:9092,cp03.woolford.io:9092";
     public static String APPLICATION_ID = "kstreams-rtd-streamer";
-    public static String SCHEMA_REGISTRY_URL = "http://cp01.woolford.io:8081";
+    public static String SCHEMA_REGISTRY_URL = "http://cp01.woolford.io:8081,http://cp02.woolford.io:8081";
 
 }
