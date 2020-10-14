85a43f18cf7173afa95930f43b1bed23374651fa
Alex Woolford <alex@woolford.io>
2020-10-13 22:50:56 -0600

    bumped versions

--- rtd-feed/src/main/java/io/woolford/rtd/feed/KafkaConfig.java
+++ rtd-feed/src/main/java/io/woolford/rtd/feed/KafkaConfig.java
@@ -8,7 +8,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
-import org.springframework.kafka.support.serializer.JsonSerializer;
 
 import java.util.HashMap;
 import java.util.Map;
